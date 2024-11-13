981cb508d0a181f63c528e244a01074c37ed6527
warlockja <warlockja@gmail.com>
2024-11-13 21:05:10 +0300

    added search in Vectorize logic

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,51 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {
+	VECTORIZE: Vectorize;
+	AI: Ai;
+	ACCESS_KEY: string;
+}
+interface EmbeddingResponse {
+	shape: number[];
+	data: number[][];
+}
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		// checking for accesskey header
+		const headers = new Headers(request.headers);
+		const keyHeader = headers.get('x-access-key');
+		if (keyHeader !== env.ACCESS_KEY) {
+			return new Response('access denied', { status: 403 });
+		}
+
+		// url params
+		const url = new URL(request.url);
+
+		const path = url.pathname;
+		if (path.startsWith('/favicon')) {
+			return new Response('', { status: 404 });
+		}
+
+		// search query params
+		// query param for vectorized query
+		const query = url.searchParams.get('query');
+		const topK = Number(url.searchParams.get('topK')) ?? 1;
+
+		if (!query) {
+			return new Response('insufficient data', { status: 400 });
+		}
+
+		// generating query vector
+		const queryVector: EmbeddingResponse = await env.AI.run('@cf/baai/bge-base-en-v1.5', {
+			text: [query],
+		});
+
+		const matches = await env.VECTORIZE.query(queryVector.data[0], {
+			topK,
+		});
+
+		return Response.json({
+			// cosine distance metric, where the closer to one, the more similar.
+			matches: matches,
+		});
 	},
 } satisfies ExportedHandler<Env>;
